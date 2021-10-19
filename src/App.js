36cf64f708d4fc59419d4d6b77a6152fe140e4eb
awais-shafiq <70453600+awais-shafiq@users.noreply.github.com>
2021-10-19 19:53:49 +0500

    Made few changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AuthRoute from "./AuthRoutes";
+import ProtectedRoute from "./ProtectedRoutes";
 import AppContextProvider from "./context/AppContext";
 import "./styles.css";
-import { Routes } from "./utils/routes";
+import { Routes, PathType } from "./utils/routes";
 
 export default function App() {
   return (
@@ -10,11 +12,30 @@ export default function App() {
       <BrowserRouter>
         <Switch>
           <AppContextProvider>
-            {Routes.paths.map((path, index) => (
-              <Route exact={path.exact} path={path.prefix} key={index}>
-                {<path.component pageNo={index} />}
-              </Route>
-            ))}
+            {Routes.paths.map((path, index) => {
+              if (path.pathType === PathType().AUTH) {
+                return (
+                  <AuthRoute exact={path.exact} path={path.pathname}>
+                    {<path.component pageNo={index} />}
+                  </AuthRoute>
+                );
+              } else if (path.pathType === PathType().PROTEDTED) {
+                return (
+                  <ProtectedRoute exact={path.exact} path={path.pathname}>
+                    {<path.component pageNo={index} />}
+                  </ProtectedRoute>
+                );
+              } else {
+                return (
+                  <Route exact={path.exact} path={path.pathname}>
+                    {<path.component pageNo={index} />}
+                  </Route>
+                );
+              }
+              // <Route exact={path.exact} path={path.prefix} key={index}>
+              //   {<path.component pageNo={index} />}
+              // </Route>
+            })}
           </AppContextProvider>
         </Switch>
       </BrowserRouter>
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,43 +1,15 @@
 import React, { createContext, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { refreshToken } from "../redux/reducers/AuthReducer";
-import { Routes } from "../utils/routes";
 
 const AppContext = createContext();
 
 export default function AppContextProvider({ children }) {
-  const auth_state = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const history = useHistory();
 
   useEffect(() => {
     dispatch(refreshToken());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (!auth_state.access_token) {
-      return;
-    }
-
-    Routes.paths.forEach((path) => {
-      const shouldOpenDashboard =
-        path.isAuthenticated &&
-        auth_state.is_user_authenticated &&
-        history.location.pathname.includes(path.prefix);
-
-      const shouldOpenLandingPage =
-        path.isAuthenticated &&
-        !auth_state.is_user_authenticated &&
-        history.location.pathname.includes(path.prefix);
-
-      if (shouldOpenDashboard) {
-        history.push("/dashboard");
-      } else if (shouldOpenLandingPage) {
-        history.push("/");
-      }
-    });
-  }, [auth_state, history]);
 
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 }

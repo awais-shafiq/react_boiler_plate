515ea2118f7cc74621ba7b2c7c77c5fdf85fa806
awais-shafiq <awaisshafiq503@gmail.com>
2021-10-28 23:15:22 +0500

    Main Routes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,76 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AuthRoute from "./AuthRoutes";
-import ProtectedRoute from "./ProtectedRoutes";
-import AppContextProvider from "./context/AppContext";
+import RoutesWrapper from "./RoutesWrapper";
 import "./styles.css";
-import { Routes, PathType } from "./utils/routes";
+import { MainRoutes, PathType } from "./utils/routes";
+import { useSelector } from "react-redux";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="loader-circle" />
-      <BrowserRouter>
-        <Switch>
-          <AppContextProvider>
-            {Routes.paths.map((path, index) => {
-              if (path.pathType === PathType().AUTH) {
-                return (
-                  <AuthRoute exact={path.exact} path={path.pathname}>
-                    {<path.component pageNo={index} />}
-                  </AuthRoute>
-                );
-              } else if (path.pathType === PathType().PROTEDTED) {
-                return (
-                  <ProtectedRoute exact={path.exact} path={path.pathname}>
-                    {<path.component pageNo={index} />}
-                  </ProtectedRoute>
-                );
-              } else {
-                return (
-                  <Route exact={path.exact} path={path.pathname}>
-                    {<path.component pageNo={index} />}
-                  </Route>
-                );
-              }
-              // <Route exact={path.exact} path={path.prefix} key={index}>
-              //   {<path.component pageNo={index} />}
-              // </Route>
-            })}
-          </AppContextProvider>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+
+	const application = useSelector((state) => state.application);
+
+	return (
+		<div className="App">
+
+			<BrowserRouter>
+				<Switch>
+					<RoutesWrapper>
+						{
+
+							Object.values(MainRoutes).map((route, index) => {
+								return (
+
+									<Route exact={route.exact} path={route.pathname} key={index}>
+										<route.component />
+										{/* {
+											route.has_nested_routes && (
+												route.nested_routes.map((nested_route, idx) => {
+													if (nested_route.required_roles.includes(application.role.name)) {
+														return (
+															<Switch>
+																<Route exact={nested_route.exact} path={`${route.pathname}${nested_route.pathname}`} key={idx}>
+																	{<nested_route.component />}
+																</Route>
+															</Switch>
+														)
+													} else {
+														return (
+															<Route exact={nested_route.exact} path={`${route.pathname}${nested_route.pathname}`} key={idx}>
+																<h3>You do not have permission to access this page</h3>
+															</Route>
+														)
+													}
+												})
+											)
+										} */}
+									</Route>
+
+								)
+							})
+
+							// Routes.paths.map((path, index) => {
+
+							// 	return (
+							// 		<Route exact={path.exact} path={path.pathname} key={index}>
+							// 			<path.component pageNo={index} />
+							// 			{
+							// 				path.sub_routes && path.sub_routes.length > 0 && (
+							// 					path.sub_routes.map((sub_path, idx) => {
+							// 						return (
+							// 							<Route path={`${path.pathname}${sub_path.pathname}`} key={idx}>
+							// 								{<sub_path.component pageNo={index} />}
+							// 							</Route>
+							// 						)
+							// 					})
+							// 				)
+							// 			}
+							// 		</Route>
+							// 	)
+
+							// })
+						}
+					</RoutesWrapper>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
